b1839ad57ca2bf462bdeb8cb3ad3bdb598013e01
ACECWB <549971521@qq.com>
2020-07-03 09:12:04 +0800

    test

--- src/util/DBUtil.java
+++ src/util/DBUtil.java
@@ -1,5 +1,20 @@
 package util;
 
-public class DBUtil {
+import java.sql.Connection;
 
+public class DBUtil {
+	private static final String jdbcUrl="jdbc:mysql://localhost:3306/takeOut?useUnicode=true&characterEncoding=UTF-8";
+	private static final String dbUser="root";
+	private static final String dbPwd="123456";
+	static{
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	public static Connection getConnection() throws java.sql.SQLException{
+		return java.sql.DriverManager.getConnection(jdbcUrl, dbUser, dbPwd);
+	}
 }
